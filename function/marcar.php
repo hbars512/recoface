3452794c76eccdd5ce01a7df033b74f56ad1e2f7
Herbert Arias <dayanqwe123@gmail.com>
2019-10-28 13:27:03 -0500

    Marcar usando un fichero

--- function/marcar.php
+++ function/marcar.php
@@ -1,25 +1,39 @@
 <?php
 
-date_default_timezone_set('America/Lima');
-$var = ['idAsistencia'];
+$n_archivo="code.txt";
+shell_exec("sort -u $n_archivo -o $n_archivo");
 
-for ($i=0; $i < count($var); $i++) {
-    $_POST[$var[$i]] = trim($_POST[$var[$i]]);
-    if ( !isset($_POST[$var[$i]]) ) return header("location: ../index.php");
-    if ( empty($_POST[$var[$i]]) )  return header("location: ../index.php");
-}
-
-$dateNow = new DateTime();
-$timestamp = $dateNow->format('Y-m-d H:i:s');
-
-$sql = "INSERT INTO asistencia (idAsistencia, fechaAsistencia) VALUES (:COD, :FAS)";
+$fp = fopen($n_archivo, "r");
 
 require 'conexion.php';
 $conn = dbConnect();
-$resultado = $conn->prepare($sql);
+date_default_timezone_set('America/Lima');
+$var = ['idAsistencia'];
 
-$resultado->execute([":COD"   => $_POST[$var[0]],
-                     ":FAS"   => $timestamp
-                    ]);
+// for ($i=0; $i < count($var); $i++) {
+//   $_POST[$var[$i]] = trim($_POST[$var[$i]]);
+//   if ( !isset($_POST[$var[$i]]) ) return header("location: ../index.php");
+//   if ( empty($_POST[$var[$i]]) )  return header("location: ../index.php");
+// }
+
+
+while (!feof($fp)){
+      $codigo = fgets($fp);
+
+      $dateNow = new DateTime();
+      $timestamp = $dateNow->format('Y-m-d H:i:s');
+
+      $sql = "INSERT INTO asistencia (idAsistencia, fechaAsistencia) VALUES (:COD, :FAS)";
+
+      $resultado = $conn->prepare($sql);
+
+      $resultado->execute([":COD"   => $codigo,
+        ":FAS"   => $timestamp
+      ]);
+}
+fclose($fp);
+
+unlink($n_archivo);
+
 
 header("location: ../index.php");
--- marcar.php
+++ marcar.php
@@ -12,15 +12,15 @@ include "./function/header.php";
 
         <!-- Text input-->
 
-        <div class="form-group">
-            <label class="col-md-4 control-label">Codigo</label>
-            <div class="col-md-4 inputGroupContainer">
-                <div class="input-group">
-                    <span class="input-group-addon"><i class="glyphicon glyphicon-align-center"></i></span>
-                    <input onkeypress="solonum()" onpaste="alert('No puedes pegar');return false" id="codigo" minlength="7" maxlength="8" required="" name="idAsistencia" placeholder="Ingrese su Codigo" class="form-control"  type="text">
-                </div>
-            </div>
-        </div>
+        <!-- <div class="form&#45;group"> -->
+        <!--     <label class="col&#45;md&#45;4 control&#45;label">Codigo</label> -->
+        <!--     <div class="col&#45;md&#45;4 inputGroupContainer"> -->
+        <!--         <div class="input&#45;group"> -->
+        <!--             <span class="input&#45;group&#45;addon"><i class="glyphicon glyphicon&#45;align&#45;center"></i></span> -->
+        <!--             <input onkeypress="solonum()" onpaste="alert('No puedes pegar');return false" id="codigo" minlength="7" maxlength="8" required="" name="idAsistencia" placeholder="Ingrese su Codigo" class="form&#45;control"  type="text"> -->
+        <!--         </div> -->
+        <!--     </div> -->
+        <!-- </div> -->
 
         <div class="text-center">
             <div style="display: inline-block">
@@ -32,14 +32,14 @@ include "./function/header.php";
                     </div>
                 </div>
             </div>
-            <div style="display: inline-block">
-                <div class="form-group">
-                    <label class="col-md-4 control-label"></label>
-                    <div class="col-md-4">
-                        <button type="reset" class="btn btn-default" >Limpiar <span class="glyphicon glyphicon-repeat"></span></button>
-                    </div>
-                </div>
-            </div>
+            <!-- <div style="display: inline&#45;block"> -->
+            <!--     <div class="form&#45;group"> -->
+            <!--         <label class="col&#45;md&#45;4 control&#45;label"></label> -->
+            <!--         <div class="col&#45;md&#45;4"> -->
+            <!--             <button type="reset" class="btn btn&#45;default" >Limpiar <span class="glyphicon glyphicon&#45;repeat"></span></button> -->
+            <!--         </div> -->
+            <!--     </div> -->
+            <!-- </div> -->
         </div>
 
 
