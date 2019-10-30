47ec595878480ac12bea22b2d0fe89eeb95a0b69
Herbert Arias <dayanqwe123@gmail.com>
2019-10-30 05:52:10 -0500

    Solo consulta general y no específica

--- function/asistencia.php
+++ function/asistencia.php
@@ -8,15 +8,15 @@ $sql = "SELECT profesores.codigo as Codigo,
                profesores.apellido as Apellido,
                asistencia.fechaAsistencia as Asistencia
         FROM profesores INNER JOIN asistencia WHERE profesores.codigo = asistencia.idAsistencia
-        order by profesores.codigo DESC";
+        order by asistencia.fechaAsistencia DESC";
 $filas=$conn->query($sql)->fetchAll(PDO::FETCH_OBJ);
 
-$sqlid = "SELECT profesores.codigo as Codigo,
-               profesores.nombre as Nombre,
-               profesores.apellido as Apellido,
-               asistencia.fechaAsistencia as Asistencia
-        FROM profesores INNER JOIN asistencia
-        WHERE profesores.codigo = 30584665
-          AND asistencia.idAsistencia = 30584665
-        ORDER BY profesores.codigo DESC";
-$filasid = $conn->query($sql)->fetchAll(PDO::FETCH_OBJ);
+// $sqlid = "SELECT profesores.codigo as Codigo,
+//                profesores.nombre as Nombre,
+//                profesores.apellido as Apellido,
+//                asistencia.fechaAsistencia as Asistencia
+//         FROM profesores INNER JOIN asistencia
+//         WHERE profesores.codigo = 30584665
+//           AND asistencia.idAsistencia = 30584665
+//         ORDER BY profesores.codigo DESC";
+// $filasid = $conn->query($sql)->fetchAll(PDO::FETCH_OBJ);
